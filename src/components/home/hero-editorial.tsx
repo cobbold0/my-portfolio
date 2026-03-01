3a6d35d6cb2edc4928acd9d00a4ca0e41adc0b8a
Nana Kwaw Cobbold <a.kwawcobbold@gmail.com>
2026-03-01 22:53:49 +0000

    fix: profile

--- src/components/home/hero-editorial.tsx
+++ src/components/home/hero-editorial.tsx
@@ -95,7 +95,7 @@ export function HeroEditorial({ profile, linkedinProfile }: HeroEditorialProps) 
 
       {profile.profileImage ? (
         <div className="relative z-10 mx-auto w-full max-w-[320px] lg:mx-0 lg:justify-self-end">
-          <div className="overflow-hidden bg-transparent">
+          <div className="overflow-hidden border border-border bg-transparent">
             <Image
               src={profile.profileImage}
               alt={`${profile.name} profile picture`}
